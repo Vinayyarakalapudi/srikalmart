449e69e4f3aeadb5466a9e89ae09543c79d697ac
Vinayyarakalapudi <vinayyarakalapudi2006@gmail.com>
2025-08-20 00:14:56 +0530

    Update RegisterCard.js

--- client/src/components/Card/RegisterCard/RegisterCard.js
+++ client/src/components/Card/RegisterCard/RegisterCard.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import './RegisterCard.css';
-const navigate = useNavigate();
+
 const RegisterCard = () => {
     const [formData, setFormData] = useState({
         firstName: '',
