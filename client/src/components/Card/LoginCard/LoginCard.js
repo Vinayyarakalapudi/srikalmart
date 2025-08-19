5dcdee9171d7e5a9b5b127cae0cf1c711d051cfd
Vinayyarakalapudi <vinayyarakalapudi2006@gmail.com>
2025-08-19 21:53:40 +0530

    Update LoginCard.js

--- client/src/components/Card/LoginCard/LoginCard.js
+++ client/src/components/Card/LoginCard/LoginCard.js
@@ -27,7 +27,7 @@ const LoginCard = () => {
         setSuccess('');
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://srikalmart-1.onrender.com/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
