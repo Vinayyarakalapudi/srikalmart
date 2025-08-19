9dd4a5d5babe6ecb62d869e3b4368293e43c1c71
Vinayyarakalapudi <vinayyarakalapudi2006@gmail.com>
2025-08-19 21:52:57 +0530

    Update RegisterCard.js

--- client/src/components/Card/RegisterCard/RegisterCard.js
+++ client/src/components/Card/RegisterCard/RegisterCard.js
@@ -29,7 +29,7 @@ const RegisterCard = () => {
         setSuccess('');
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch('https://srikalmart-1.onrender.com/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
